908b55a4baf14b4b04eaca118e29df10b41533ff
Vinícius Araújo <viniciusaraujom@outlook.com>
2021-05-10 16:40:04 -0300

    Update uri1037.java

--- Java 11/src/uri1037.java
+++ Java 11/src/uri1037.java
@@ -28,24 +28,26 @@ public class uri1037 {
 
         numero = leitor.nextDouble();
 
-        if (numero < 0 && numero > 100){
+        if (numero < 0 || numero > 100){
             System.out.println("Fora de intervalo");
         }
 
         else if (numero <= 25.0000){
-            System.out.println("Intervalo [0, 25]");
+            System.out.println("Intervalo [0,25]");
         }
 
         else if (numero > 25 && numero <= 50.000000){
-            System.out.println("Intervalo (25, 50]");
+            System.out.println("Intervalo (25,50]");
         }
 
         else if (numero > 50 && numero <= 75.000000){
-            System.out.println("Intervalo (50, 75]");
+            System.out.println("Intervalo (50,75]");
         }
 
         else if (numero > 75 && numero <= 100.000000){
-            System.out.println("Intervalo (75, 100]");
+            System.out.println("Intervalo (75,100]");
         }
+
+        leitor.close();
     }    
 }
