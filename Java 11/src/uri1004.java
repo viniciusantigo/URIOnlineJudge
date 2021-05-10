96239c1f8be714dfc075022f4ab3229d43228cec
Vinícius Araújo <viniciusaraujom@outlook.com>
2021-05-10 06:40:18 -0300

    Add files via upload

---
+++ Java 11/src/uri1004.java
@@ -0,0 +1,31 @@
+/**
+Leia dois valores inteiros. A seguir, calcule o produto entre estes dois valores e atribua esta operação à variável PROD. 
+A seguir mostre a variável PROD com mensagem correspondente.   
+
+Entrada
+O arquivo de entrada contém 2 valores inteiros.
+
+Saída
+Imprima a mensagem "PROD" e a variável PROD conforme exemplo abaixo, com um espaço em branco antes e depois da igualdade. 
+Não esqueça de imprimir o fim de linha após o produto, caso contrário seu programa apresentará a mensagem: “Presentation Error”.
+ */
+
+import java.util.Scanner; 
+public class uri1004 {
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+
+        int a, b, prod;
+
+        a = sc.nextInt();
+        b = sc.nextInt();
+
+        prod = a * b;
+
+        System.out.print("PROD = " + prod);
+        System.out.println();
+
+        sc.close();
+    }
+}
